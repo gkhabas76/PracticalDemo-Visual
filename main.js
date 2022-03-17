3becb557170095fcbeb23b101bd2c81c35e90ca8
ParagMhrjn <chandra_parag@hotmail.com>
2022-03-17 13:47:59 +0100

    added extra section on index page

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ window.addEventListener('scroll', () =>{
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
 })
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'fa fa-plus') {
+            icon.className = "fa fa-minus"
+        }else {
+            icon.className = "fa fa-plus";
+        }
+    })
+})
